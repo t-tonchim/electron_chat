97bd591e9c0a8c291cbbcada0d38d6ff3ae77b23
tomoyuki-tanaka <masuminium5@gmail.com>
2017-04-17 22:46:25 +0900

    add main menus

--- src/main/index.js
+++ src/main/index.js
@@ -1,7 +1,10 @@
 import { app } from "electron";
+
 import createWindow from "./createWindow";
+import setAppMenu from './setAppMenu';
 
 app.on("ready", () => {
+  setAppMenu();
   createWindow();
 });
 
